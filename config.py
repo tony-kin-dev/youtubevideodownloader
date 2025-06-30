559bb2885367dd3a39bf666b8139e8799639ef9a
tony-kin-dev <sitydom@gmail.com>
2025-06-30 12:58:57 +0300

    Токен бота теперь правильно забирается

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-TELEGRAM_TOKEN = "YOUR_BOT_TOKEN_HERE"
-MAX_FILE_SIZE = 2000  # МБ
-LANGUAGES = ['ru', 'en'] 
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "YOUR_BOT_TOKEN_HERE")
+MAX_FILE_SIZE = int(os.getenv("MAX_FILE_SIZE", 2000))  # МБ
+LANGUAGES = os.getenv("LANGUAGES", "ru,en").split(",")
